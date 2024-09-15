33214981ae63ed6c8273ea3ddf1fae0048b98a95
amir-mansoor <qazimansor89@gmail.com>
2024-09-15 15:38:57 +0500

    composition

--- oop-in-python-order-of-basic-growths.py
+++ oop-in-python-order-of-basic-growths.py
@@ -3,11 +3,42 @@ class Point:
         self.x = x
         self.y = y
 
-p1 = Point() 
-print("p1 ",p1.x)
+    def __str__(self):
+        return "[" + str(self.x) + "," + str(self.y) + "]"
 
-p2 = Point(2,4)
-print("p2", p2.x)
+    # def get_human_readable(self):
+    #     return "[" + str(self.x) + "," + str(self.y) + "]"
 
-print(p2) # here is the pointer, technically these are same things 
+# p1 = Point() 
+# print("p1 ",p1.x)
 
+# p2 = Point(2,4)
+# print("p2", p2.x)
+
+# print(p2) # here is the pointer, technically these are same things
+# print(p2.get_human_readable())
+
+
+
+
+
+# *********** Composition *************
+
+class Shape:
+    def __init__(self,points):
+        self.points = points
+
+    def __str__(self):
+        ret = ""
+        for i in self.points:
+            ret += str(i) + " - "
+
+        return ret
+
+p1 = Point(5,1)
+p2 = Point(2,3)
+p3 = Point(23,12)
+
+p = [p1,p2,p3]
+sh = Shape(p)
+print(sh)
